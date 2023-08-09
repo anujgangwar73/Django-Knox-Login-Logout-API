d9b8d8ab8a9596c5a2e5915ce7eb575405c08767
Anuj gangwar <anujgangwar73@gmail.com>
2023-08-10 00:10:51 +0530

    modify changes

--- userlogin/formapi/serializers.py
+++ userlogin/formapi/serializers.py
@@ -15,6 +15,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        user = User.objects.create_user(validated_data['email'], validated_data['password'])
 
         return user
